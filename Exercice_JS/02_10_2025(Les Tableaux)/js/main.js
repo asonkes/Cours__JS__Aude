70cf7a7165b5dc8422eb82cc711c02857ac32b98
Audrey Sonkes <a.sonkes@interface3.be>
2025-10-08 09:43:04 +0200

    mise à jour exercice

--- Exercice_JS/02_10_2025(Les Tableaux)/js/main.js
+++ Exercice_JS/02_10_2025(Les Tableaux)/js/main.js
@@ -26,8 +26,6 @@ button1.addEventListener("click", (event) => {
     }
 
     document.getElementById("prenom").value = "";
-    //textInfos.classList.remove("textInfos");
-    //textInfos.textContent = "";
 })
 
 
@@ -56,31 +54,29 @@ button2.addEventListener("click", (event) => {
     /** ON enlève le rechargement automatique de la page */
     event.preventDefault();
 
-    minNumber = 0;
-    maxNumber = prenoms.length;
-
-    /** Normalement :  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber */
-    /** Mais j'ai enlevé le +1 car amène des erreurs je crois, à vérifier !!! */
-    number = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-    console.log("Le chiffre aléatoire est de :", number);
+    // C'est un return donc il faut réaffecter le number à une variable
+    // On doit pas mettre number en paramètre, car tu n'as besoin d'aucune données pour calculer le random
+    // à la base.
+    let number = random();
 
     textIndex.classList.add("active");
     textIndex.textContent = `Félicitations ${prenoms[number]}, tu t'occuperas de la maison toute la semaine.`;
 
     /** Ici normalement, le nom de la personne au sort doit être supprimée */
-    /** Le tableau se met à jour en console.log */
-    /** Faut le mettre à jour visuellement sur la page */
-    prenoms.splice(prenoms[number], 1);
+    prenoms.splice(number, 1);
+
+    /** En console.log(résultat juste), on touche plus au splice() */
     console.log(prenoms);
 
-    textTableau.textContent = `${prenoms}`;
+    let array = prenoms;
 
+    textTableau.textContent = array.join(" | ");
 
-
+    if(array.length === 0) {
+        textTableau.textContent = `Il n'y a plus de prénoms dans le tableau`;
+        button2.disabled = true;
+    }
 })
-
-
-
 
 //////////////////////////////////////////////////
 /** Création de fonctions pour diminuer le code */
@@ -105,3 +101,18 @@ function writeError() {
     /** On ajoute le contenu */
     form.appendChild(textInfos);
 }
+
+function random() {
+    minNumber = 0;
+    maxNumber = prenoms.length;
+
+    /** Normalement :  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber */
+    /** Mais j'ai enlevé le +1 car amène des erreurs je crois, à vérifier !!! */
+    number = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+    console.log("Le chiffre aléatoire est de :", number);
+
+    return number;
+}
+
+
+// Pour supprimer un élément du DOm ==> remove.
