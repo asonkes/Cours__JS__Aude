5d9e4c0035a313575dde4bc490f2c6524e40a328
asonkes <audrey.sonkes@gmail.com>
2025-10-09 22:40:09 +0200

    second commit

--- Exercice_JS/09_10_2025(Les boucles)/js/main.js
+++ Exercice_JS/09_10_2025(Les boucles)/js/main.js
@@ -1,6 +1,34 @@
 /** On initialise les variables dont on a besoin */
-const button = document.getElementById("button");
-console.log(button);
-
+/** La variable poru l'input */
 const input = document.getElementById("input");
-console.log(input);
+/** La variable pour le bouton */
+const button = document.getElementById("button");
+
+const tab = ["Maine Coon Noir", "Maine Coon Blanc", "Maine Coon Roux", "Maine Coon Bleu", "Miane Coon Crème", "Maine Coon écaille de Tortue"];
+
+button.addEventListener("click", () => {
+    /** La variable pour récupérer la valeur de l'input */
+    let inputValue = input.value;
+    /** On récupère l'élément parent pour la création du nouvel élément */
+    const container = document.querySelector(".container");
+
+    /** Ici on fait en sorte avec le while de bloqué l'ajout de données */
+    // Tant que c'est un nombre
+    // Tant que la valeur ajoutée est vide
+    if (inputValue === "") {
+        const nouvelElement = document.createElement("p");
+        nouvelElement.classList.add("text");
+        nouvelElement.textContent=`Vous avez oublié de mettre une valeur 😡​😡​😡​ !!!`;
+        container.appendChild(nouvelElement);
+        console.log(inputValue);
+    } else {
+        const list = document.querySelector(".list");
+        console.log(list);
+
+        /** La variable pour afficher le tableau */
+        list.innerhtml = `${tab}`; 
+
+        console.log("Vous avez entré une valeur correcte");
+    }
+})
+
