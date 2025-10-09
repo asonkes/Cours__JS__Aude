c5c5034917092f21888409c01828448b5a1f057c
Audrey Sonkes <a.sonkes@interface3.be>
2025-10-09 16:43:59 +0200

    first commit exercice

---
+++ Exercice_JS/09_10_2025(Les boucles)/js/main.js
@@ -0,0 +1,6 @@
+/** On initialise les variables dont on a besoin */
+const button = document.getElementById("button");
+console.log(button);
+
+const input = document.getElementById("input");
+console.log(input);
