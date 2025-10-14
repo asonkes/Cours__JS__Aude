babd82a7f5c893ffbe5821b779c410f44b93e8b7
asonkes <audrey.sonkes@gmail.com>
2025-10-14 22:57:02 +0200

    first commit

---
+++ Exercice_JS/14_10_2025(Exercice_boucle_suite)/js/main.js
@@ -0,0 +1,109 @@
+/** On va initialiser les variables dont on a besoin */
+/** Le tableau des mots */
+const tab = ["lion", "tigre", "elephant", "ane", "chevreuil", "blaireau"];
+
+/** On doit initialiser la variable de l'input */
+const input = document.querySelector(".text input");
+
+/** On initialise la variable du bouton "jouer" */
+const button = document.querySelector(".text button");
+
+/** On initialise le nbr min et max, même sans valeur */
+let nbrMin;
+let nbrMax;
+
+const randomNumber = generateRandomNumber();
+generateHiddenWord();
+
+/** On fait ici l'évènement au click */
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    playGame();
+})
+
+    /** On fait ici l'évènement au clavier */
+input.addEventListener("keyup", (event) => {
+    event.preventDefault();
+
+    playGame();
+})
+
+function playGame() {
+    /** Ici cela permet que s'il y eu une erreur avant */
+    /** Qd on tape une lettre, l'erreur disparaisse */
+    const error = document.querySelector(".error");
+    if(error) {
+        error.remove();
+    }
+
+    /** On doit initialiser la valeur de l'input */
+    /** On enleve les espaces devant et derrière le mot */
+    const inputValue = input.value.trim().toLowerCase();
+    console.log(inputValue);
+
+    if(isNaN(inputValue) && inputValue !== '') {
+        console.log("ok, tu as rentré une lettre");
+    } else {
+        errorText();
+    }
+
+    document.querySelector(".text input").value = "";
+}
+
+
+/////////////////////////////
+/** Création des fonctions */
+/////////////////////////////
+function generateRandomNumber() {
+
+    /** on doit initialiser le nombre minimum et maximum */
+    nbrMin = 0;
+    nbrMax = tab.length;
+
+    /** Création du mot aléatoire */
+    let random = Math.floor(Math.random() * (nbrMax - nbrMin + 1)) + nbrMin;
+    console.log("nombre aléatoire :", random);
+
+    return random;
+}
+
+function generateHiddenWord() {
+
+    /** On doit afficher le mot de la liste selon le nombre aléatoire */
+    let motText = tab[randomNumber];
+    console.log("le mot est :", motText);
+
+    /** On initialise l'élément dans lequel on va afficher le texte */
+    const mot = document.getElementById("mot");
+
+    // On va créer les éléments enfants
+    const text = document.createElement("p");
+    const span = document.createElement("span");
+
+    // On va insérer le texte dans l'élément "p"
+    text.textContent = `Le mot à découvrir est : `;
+
+    // On va insérer le texte dans l'élément "span"
+    for(let i = 0; i < motText.length; i++) {
+        span.textContent += ` _ `;
+    }
+
+    /** Ici, on utilise "append" et pas "appendchild" car on va créer plusieurs éléments */
+    mot.append(text, span);
+}
+
+function errorText() {
+
+    const error = document.querySelector(".error");
+    if(error) {
+        return;
+    }
+
+    /** On va créer l'élément pour afficher le texte erreur */
+    const errorElement = document.createElement("p");
+    errorElement.classList.add('error');
+    errorElement.textContent = `* Vous vous êtes trompé, insérez un MOT !!!`;
+    /** Ici on insère l'élément après l'input */
+    input.insertAdjacentElement('afterend', errorElement);
+}
---
+++ Exercice_JS/14_10_2025(Exercice_boucle_suite)/readme.md
@@ -0,0 +1,16 @@
+Logique :
+
+1. Si lettre trouvée, elle doit s'afficher
+
+//Il faut faire un forEach sur toutes les lettres
+// If (inputValue === "une des lettres du mot") {
+    afficher la lettre
+}
+
+2. Décider un nombre de chances
+3. Si nombre de chances dépassée, perdu
+
+
+
+
+??? Question à Aude, un mot avec 2 lettres "p" ???
